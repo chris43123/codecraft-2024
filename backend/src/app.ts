7513c67fbe8c342fcbc1c3e974d168f57d13c0bf
Chris Alexander Andino Godoy <46337972+chris43123@users.noreply.github.com>
2024-09-27 10:16:22 -0600

    register routes in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,20 @@ import express from 'express'
 import {db} from './config/mssql'
 import { expressjwt } from 'express-jwt'
 
+import * as auth from './routes/auth'
+import * as categories from './routes/categories'
+import * as tasks from './routes/task'
+import * as tags from './routes/tags'
+
 const app = express()
 
 app.use(express.json());
-app.use('/', (req, res, next) => {} )
+
+app.use('/api/auth', auth.router)
+app.use('/api/categories', categories.router)
+app.use('/api/tasks', tasks.router)
+app.use('/api/tags', tags.router)
+
 app.listen(process.env.port || 3000, async () => {
     const pool = await db()
     
